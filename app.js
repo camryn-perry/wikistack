7e94f7f978b359f326ae8e6d5034fc969b618bce
Camryn Perry <camrynpaigeperry@gmail.com>
2019-09-26 12:00:20 -0400

    rendered main page

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const morgan = require('morgan');
-
+//const layout = require('./views/layout');
+const views = require('./views');
+console.log(views);
 const app = express();
 
 app.use(morgan('dev'));
 //how do we use this in the future?
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({extended : false}));
 
 app.get('/', (req, res) => {
-  res.send("Hello World!");
+  res.send(views.main());
 })
 
 const PORT = 1337;
