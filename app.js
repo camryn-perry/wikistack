327ba3a0be5ea4bef34d83b3f87da7e8fbadde3b
Camryn Perry <camrynpaigeperry@gmail.com>
2019-09-26 12:23:02 -0400

    added database

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const morgan = require('morgan');
-//const layout = require('./views/layout');
 const views = require('./views');
-console.log(views);
+const { db } = require('./models');
 const app = express();
 
 app.use(morgan('dev'));
 //how do we use this in the future?
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({extended : false}));
+
+db.authenticate().
+then(() => {
+  console.log('connected to the database');
+})
 
 app.get('/', (req, res) => {
   res.send(views.main());
